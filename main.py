6b1ac373af1164c399f49b100731714af3ca52a4
Moises Monter <moises.monteiro2077@hotmail.com>
2023-01-11 15:59:27 -0300

    "commit"

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 from labirinto import Labirinto
 from select_position import Select_Position
-class Trapped_Mouse(Select_Position):
+
+
+import platform
+import os
+class Config:
+    def __init__(self):
+        pass
+
+
+    def Limpar(self):
+        if platform.system() == "Windows":
+            os.system('cls')
+        else:
+            os.system('clear')
+
+
+
+
+class Trapped_Mouse(Select_Position,Config):
     def __init__(self):
         self.labirint = Labirinto()
 
     def __str__(self):
-        #[print(x,'\n') for x in self.labirint]
-        print(Trapped_Mouse())
+        
+        while True:
+            self,
+            info =Select_Position().Move()
+            if info == 'esc':
+                break
+            elif info == '5':
+                print('enter')
+                break
+
+            if info != None:
+                self.Limpar()
+                print(info)
         return '0'
 
 
 if __name__ == "__main__":
-    
-    Select_Position().Move('x')
-    
+    Trapped_Mouse().__str__()
+
+
