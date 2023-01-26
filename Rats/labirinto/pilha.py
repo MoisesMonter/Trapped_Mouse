9a996a5eec51b3e034d8c8d5ac9544bd47e14d1d
Moisés Alexandre Monteiro Araujo <97893793+MoisesMonter@users.noreply.github.com>
2023-01-26 17:30:21 +0000

    "main"

--- Rats/labirinto/pilha.py
+++ Rats/labirinto/pilha.py
@@ -13,38 +13,92 @@ class Local_Actions:
         self.Map = L
         self.Mod_Map = L
         self.Pile = []
-        pass
 
+
+    def Wall(self,location):
+        try:
+            if location == 'Top':
+                if(self.Map[int(self.Rato[0])-1][int(self.Rato[0])] == True or self.Map[int(self.Rato[0])-1][int(self.Rato[0])] == 'Q'):
+                    return True
+            if location == 'Bottom':
+                if (self.Map[int(self.Rato[0])+1][int(self.Rato[0])] == True or self.Map[int(self.Rato[0])+1][int(self.Rato[0])] == 'Q') :
+                    return True
+            if location == 'Left':
+                if (self.Map[int(self.Rato[0])][int(self.Rato[0]-1)] == True or self.Map[int(self.Rato[0])-1][int(self.Rato[0])] == 'Q'):
+                    return True
+            if location == 'Right':
+                if (self.Map[int(self.Rato[0])][int(self.Rato[0]+1)] == True or self.Map[int(self.Rato[0])+1][int(self.Rato[0])] == 'Q'):
+                    return True
+        except:
+            print("Can't to moove to left")
     def Top(self):
+        info = 'Top'
             #[0]L [0]c    
         if int(self.Rato[0]) != 0: 
-            [print(l) for l in self.Map]
-            self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
-            self.Rato[0]= int(self.Rato[0]) -1   
-            self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
-            [print(l) for l in self.Map]
-            self.pilha.append
+            if self.Wall(info) != True:   
+                [print(l) for l in self.Map]
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
+                self.Rato[0]= int(self.Rato[0]) -1   
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
+                [print(l) for l in self.Map]
+                self.Pile.append('Top')
+            else:
+                print("have an Wall")
         else:
             print("Can't to moove to up")
             
 
     def Bottom(self):
+        info ='Bottom'
             #[0]L [0]c    
         if int(self.Rato[0]) != len(self.Map)-1: 
-            [print(l) for l in self.Map]
-            print('\n')
-            self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
-            self.Rato[0]= int(self.Rato[0]) +1   
-            self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
-            [print(l) for l in self.Map]
+            if self.Wall(info) != True:        
+                [print(l) for l in self.Map]
+                print('\n')
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
+                self.Rato[0]= int(self.Rato[0]) +1   
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
+                [print(l) for l in self.Map]
+                self.Pile.append('Bottom')
+            else:
+                print("have an Wall")
         else:
             print("Can't to moove to down")
 
     def Left(self):
-        pass
+        info ='left'
+        #[0]L [0]C
+        if int(self.Rato[1]) != len(self.Map)-1: 
+            if self.Wall(info) != True:            
+                [print(l) for l in self.Map]
+                print('\n')
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
+                self.Rato[1]= int(self.Rato[0]) -1   
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
+                [print(l) for l in self.Map]
+                self.Pile.append('Left')
+            else:
+                print("have an Wall")
+        else:
+            print("Can't to moove to left")
 
     def Right(self):
-        pass
+        info ='Right'
+        #[0]L [0]C
+        if int(self.Rato[1]) != len(self.Map)-1: 
+            
+            if self.Wall(info) != True:
+                [print(l) for l in self.Map]
+                print('\n')
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]=False 
+                self.Rato[1]= int(self.Rato[1]) +1   
+                self.Map[int(self.Rato[0])][int(self.Rato[1])]= 'Rato'
+                [print(l) for l in self.Map]
+                self.Pile.append('Right')
+            else:
+                print("have an Wall")
+        else:
+            print("Can't to moove to Right")
 
     def main(self):
         pass
@@ -53,6 +107,20 @@ if __name__ == "__main__":
     Q = [5,5]
     L = [[False,True,True],['Rato',True,True],[False,False,'Q']]
     print(len(L))
-    print(Local_Actions(R,Q,L).Bottom())
+
+    print(Local_Actions(R,Q,L).Right())
+    print('\n')
+    print(Local_Actions(R,Q,L).Right())
     print('\n')
     print(Local_Actions(R,Q,L).Bottom())
+    print('\n')
+    print(Local_Actions(R,Q,L).Left())
+    print('\n')
+    print(Local_Actions(R,Q,L).Left())
+    '''print('\n')
+    print(Local_Actions(R,Q,L).Right())
+    print('\n')
+    print(Local_Actions(R,Q,L).Right())
+    print('\n')
+    print(Local_Actions(R,Q,L).Right())
+    print('\n')'''
